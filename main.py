69c97fbbe8c88ac4e8bfb1770cdfce72c6e99d0c
Paulina Radomska <113101087+pradomska@users.noreply.github.com>
2024-05-11 14:50:06 +0200

    Update main.py by add pydentic

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ Dane pogodowe uzyskasz z API: https://danepubliczne.imgw.pl/api/data/synop
 """
 
 from jinja2 import Environment, FileSystemLoader
-from unidecode import unidecode
-from urllib.parse import urljoin
+from pydantic import BaseModel
 import numpy as np
 import requests
 import pprint
@@ -18,18 +17,28 @@ import pprint
 BASE_URL = "https://danepubliczne.imgw.pl/api/data/synop/"
 
 
-def get_weather_data():
+class WeatherStationResponse(BaseModel):
+    stacja: str
+    data_pomiaru: str | None
+    godzina_pomiaru: str | None
+    temperatura: float
+    predkosc_wiatru: float | None
+    kierunek_wiatru: str | None
+    wilgotnosc_wzgledna: float | None
+    suma_opadu: float
+    cisnienie: float
+
+
+def get_weather_data_response():
     response = requests.get(BASE_URL)
     return response.json()
 
 
-def get_unique_weather_station(weather_data):
-    weather_station = [i["stacja"].upper() for i in weather_data]
-    unique_station_names = np.unique(weather_station)
-    return list(unique_station_names)
+def get_unique_weather_station_names(weather_data_response):
+    return list(np.unique([i["stacja"].upper() for i in weather_data_response]))
 
 
-def input_name_station(station_names_list):
+def input_name_station_by_user(station_names_list):
     while True:
         pprint.pp(station_names_list)
         station_name = input('Wybierz stację z wymienionych: ')
@@ -37,26 +46,28 @@ def input_name_station(station_names_list):
             return station_name
 
 
-def get_chosen_station_data(station_name):
-    station_name = unidecode(station_name)
-    response = requests.get(urljoin(BASE_URL, f'station/{station_name}'))
-    return response.json()
+def get_chosen_station_data(station_name, weather_data_response):
+    station_name = f'{station_name.title()}'
+    for element in weather_data_response:
+        if element['stacja'] == station_name:
+            print(WeatherStationResponse(**element))
+            return WeatherStationResponse(**element)
 
 
-def generate_html_template(station_response):
+def render_html_report(weather_station_response):
     file_loader = FileSystemLoader('./weather_template')
     env = Environment(loader=file_loader)
     template = env.get_template('template.html')
 
-    rendered_template = template.render(stacja=station_response['stacja'],
-                                        data_pomiaru=station_response["data_pomiaru"],
-                                        godzina_pomiaru=station_response["godzina_pomiaru"],
-                                        temperatura=station_response["temperatura"],
-                                        predkosc_wiatru=station_response["predkosc_wiatru"],
-                                        kierunek_wiatru=station_response["kierunek_wiatru"],
-                                        wilgotnosc_wzgledna=station_response["wilgotnosc_wzgledna"],
-                                        suma_opadu=station_response["suma_opadu"],
-                                        cisnienie=station_response["cisnienie"]
+    rendered_template = template.render(stacja=weather_station_response.stacja,
+                                        data_pomiaru=weather_station_response.data_pomiaru,
+                                        godzina_pomiaru=weather_station_response.godzina_pomiaru,
+                                        temperatura=weather_station_response.temperatura,
+                                        predkosc_wiatru=weather_station_response.predkosc_wiatru,
+                                        kierunek_wiatru=weather_station_response.kierunek_wiatru,
+                                        wilgotnosc_wzgledna=weather_station_response.wilgotnosc_wzgledna,
+                                        suma_opadu=weather_station_response.suma_opadu,
+                                        cisnienie=weather_station_response.cisnienie
                                         )
 
     with open("weather_template/output.html", "w", encoding='utf-8') as f:
@@ -64,11 +75,11 @@ def generate_html_template(station_response):
 
 
 def main():
-    weather_data = get_weather_data()
-    station_names = get_unique_weather_station(weather_data)
-    chose_station = input_name_station(station_names)
-    chosen_station_weather_data = get_chosen_station_data(chose_station)
-    generate_html_template(chosen_station_weather_data)
+    weather_data_response = get_weather_data_response()
+    station_names = get_unique_weather_station_names(weather_data_response)
+    chose_station = input_name_station_by_user(station_names)
+    chosen_station_weather_data = get_chosen_station_data(chose_station, weather_data_response)
+    render_html_report(chosen_station_weather_data)
 
 
 if __name__ == "__main__":
